308251c1d9f9fc73290555b00de544ec064e1178
alxmart <alxmartinez@gmail.com>
2022-01-14 16:32:10 -0300

    Cancel the notification.

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -26,6 +26,7 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.*
 import com.example.android.eggtimernotifications.receiver.AlarmReceiver
 import com.example.android.eggtimernotifications.R
+import com.example.android.eggtimernotifications.util.cancelNotifications
 import com.example.android.eggtimernotifications.util.sendNotification
 import kotlinx.coroutines.*
 
@@ -129,6 +130,13 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 */
 
                 // TODO: Step 1.15 call cancel notification
+
+                val notificationManager =
+                    ContextCompat.getSystemService(
+                        app,
+                        NotificationManager::class.java
+                    ) as NotificationManager
+                notificationManager.cancelNotifications()
 
                 AlarmManagerCompat.setExactAndAllowWhileIdle(
                     alarmManager,
